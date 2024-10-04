004bcea7f98101beca48e527ae93a59020564075
Fidelis-Muteti <fidelmwovi@gmail.com>
2024-10-05 00:55:20 +0300

    js tasklister pj

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+let myform = document.getElementById("create-task-form");
+
+myform.addEventListener("submit",(e) => {
+e.preventDefault()
+
+
+let item = document.getElementById("new-task-description").value;
+
+let ulist = document.getElementById("tasks");
+let listItem = document.createElement("li");
+listItem.textContent = item;
+
+ulist.appendChild(listItem)
+
+document.getElementById('new-task-description').value = '';
+
+
+  });
 });
+
+
